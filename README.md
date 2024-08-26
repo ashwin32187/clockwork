8901c68c4de5c3932cdc3800ec17b99a22fc6645
Ashwin Kumar Thirunavukkarasu <ashwin321@gmail.com>
2024-08-26 19:54:50 +0530

    package changes with test file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # clockwork
-A scheduler in go with customizable schedules and using time package
+A scheduler in go with customizable schedules and using time package.
+
+Please see clockwork_test.go for details on usage examples or comments on each method.
+Please see DayOfTheMonthSchedule for an example custom schedule implementation, that can be used by scheduler.
+
+Run go test -v ./... to see output for test file.
--- clockwork_test.go
+++ clockwork_test.go
@@ -1,35 +1,35 @@
-package main
+package clockwork
 
 import (
-	"clockwork/clockwork"
 	"fmt"
+	"testing"
 	"time"
 )
 
-func main() {
+func Test_clockwork(_ *testing.T) {
 	testDayOfMonthScheduler()
 	testRunner()
 }
 func testRunner() {
 	yearlyAfter, yearlyBefore, yearlyLocal, yearly, monthlyAfter, monthlyBefore, monthly, weeklyAfter, weeklyBefore, weekly,
 		dailyAfter, dailyBefore, daily, hourly, everyMinute, everySecond, dayOfTheMonth := testData()
-	fmt.Println("tasks added", clockwork.AddTasks([]*clockwork.Task{yearlyAfter, yearlyBefore, yearlyLocal, yearly, monthlyAfter, monthlyBefore, monthly, weeklyAfter, weeklyBefore, weekly,
+	fmt.Println("tasks added", AddTasks([]*Task{yearlyAfter, yearlyBefore, yearlyLocal, yearly, monthlyAfter, monthlyBefore, monthly, weeklyAfter, weeklyBefore, weekly,
 		dailyAfter, dailyBefore, daily, hourly, everyMinute, everySecond, dayOfTheMonth}))
 	time.Sleep(5 * time.Second)
 	// cannot change tasks in execution list from outside package
-	clockwork.FetchTasks()[0] = nil
+	FetchTasks()[0] = nil
 	// still has access to task post nil assigment
-	clockwork.FetchTasks()[0].Stop()
-	fmt.Println(clockwork.FetchTasks()[0].Name)
+	FetchTasks()[0].Stop()
+	fmt.Println(FetchTasks()[0].Name)
 	time.Sleep(1 * time.Second)
-	fmt.Println(clockwork.Report())
+	fmt.Println(Report())
 	// remove a task
-	clockwork.FetchTasks()[0].RemoveTask()
-	fmt.Println(clockwork.Report())
-	newEverySecond := &clockwork.Task{
+	FetchTasks()[0].RemoveTask()
+	fmt.Println(Report())
+	newEverySecond := &Task{
 		Name:     "new every second",
 		Function: Work,
-		Schedule: &clockwork.EverySecond,
+		Schedule: &EverySecond,
 		Cron:     time.Now(),
 	}
 	// add a single task
@@ -39,143 +39,143 @@ func testRunner() {
 	newEverySecond.Stop()
 	everySecond.Stop()
 	for count := 0; count < 3; count++ {
-		fmt.Println(clockwork.Report())
+		fmt.Println(Report())
 		time.Sleep(1 * time.Second)
 		everyMinute.Stop()
 	}
 	fmt.Println("test RemoveTask()")
 	fmt.Println(newEverySecond.RemoveTask())
 	fmt.Println(newEverySecond.RemoveTask())
-	fmt.Println(clockwork.Report())
+	fmt.Println(Report())
 	fmt.Println("test Reinstate()")
 	everyMinute, flag := everyMinute.Reinstate()
 	fmt.Println(flag)
 	everyMinute, flag = everyMinute.Reinstate()
 	fmt.Println(flag)
 	time.Sleep(5 * time.Second)
-	fmt.Println(clockwork.Report())
+	fmt.Println(Report())
 	time.Sleep(1 * time.Minute)
-	fmt.Println(clockwork.Report())
+	fmt.Println(Report())
 	everyMinute.Stop()
-	clockwork.RemoveTaskByName(everyMinute.Name)
+	RemoveTaskByName(everyMinute.Name)
 	time.Sleep(5 * time.Second)
 	time.Sleep(1 * time.Minute)
-	fmt.Println(clockwork.Report())
-	clockwork.ClearTasks()
+	fmt.Println(Report())
+	ClearTasks()
 	time.Sleep(2 * time.Second)
 	everySecond.Reinstate()
 	time.Sleep(2 * time.Second)
-	fmt.Println(clockwork.Report())
-	clockwork.ClearTasks()
+	fmt.Println(Report())
+	ClearTasks()
 }
 
 func Work(name string, count int) {
 	fmt.Printf("running %s, count %d, at %s \n", name, count, time.Now().String())
 }
 
-func testData() (*clockwork.Task, *clockwork.Task, *clockwork.Task, *clockwork.Task,
-	*clockwork.Task, *clockwork.Task, *clockwork.Task, *clockwork.Task,
-	*clockwork.Task, *clockwork.Task, *clockwork.Task, *clockwork.Task,
-	*clockwork.Task, *clockwork.Task, *clockwork.Task, *clockwork.Task, *clockwork.Task) {
+func testData() (*Task, *Task, *Task, *Task,
+	*Task, *Task, *Task, *Task,
+	*Task, *Task, *Task, *Task,
+	*Task, *Task, *Task, *Task, *Task) {
 	refTime := time.Now()
-	return &clockwork.Task{
+	return &Task{
 			Name:     "yearlyAfter",
 			Function: Work,
-			Schedule: &clockwork.Yearly,
+			Schedule: &Yearly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day()+1, 00, 00, 00, 0, time.Local),
-		}, &clockwork.Task{
+		}, &Task{
 			Name:     "yearlyBefore",
 			Function: Work,
-			Schedule: &clockwork.Yearly,
+			Schedule: &Yearly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day()-1, 00, 00, 00, 0, time.Local),
-		}, &clockwork.Task{
+		}, &Task{
 			Name:     "yearlyLocal",
 			Function: Work,
-			Schedule: &clockwork.Yearly,
+			Schedule: &Yearly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day(), refTime.Hour()-1, 11, 00, 0, time.Local),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "yearly",
 			Function: Work,
-			Schedule: &clockwork.Yearly,
+			Schedule: &Yearly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day(), refTime.Hour()-5, refTime.Minute()-28, refTime.Second(), 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "monthlyAfter",
 			Function: Work,
-			Schedule: &clockwork.Monthly,
+			Schedule: &Monthly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day()+1, 07, 07, 07, 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "monthlyBefore",
 			Function: Work,
-			Schedule: &clockwork.Monthly,
+			Schedule: &Monthly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day()-1, 17, 30, 00, 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "monthly",
 			Function: Work,
-			Schedule: &clockwork.Monthly,
+			Schedule: &Monthly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day(), refTime.Hour()-5, refTime.Minute()-28, refTime.Second(), 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "weeklyAfter",
 			Function: Work,
-			Schedule: &clockwork.Weekly,
+			Schedule: &Weekly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day()+1, 07, 14, 21, 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "weeklyBefore",
 			Function: Work,
-			Schedule: &clockwork.Weekly,
+			Schedule: &Weekly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day()-2, 01, 11, 48, 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "weekly",
 			Function: Work,
-			Schedule: &clockwork.Weekly,
+			Schedule: &Weekly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day(), refTime.Hour()-5, refTime.Minute()-28, 00, 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "dailyAfter",
 			Function: Work,
-			Schedule: &clockwork.Daily,
+			Schedule: &Daily,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day(), 20, 56, 07, 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "dailyBefore",
 			Function: Work,
-			Schedule: &clockwork.Daily,
+			Schedule: &Daily,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day(), 1, 17, 59, 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "daily",
 			Function: Work,
-			Schedule: &clockwork.Daily,
+			Schedule: &Daily,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day(), refTime.Hour()-5, refTime.Minute()-28, 00, 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "hourly",
 			Function: Work,
-			Schedule: &clockwork.Hourly,
+			Schedule: &Hourly,
 			Cron:     time.Date(refTime.Year(), refTime.Month(), refTime.Day(), refTime.Hour()-5, refTime.Minute()-28, 00, 0, time.UTC),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "everyMinute",
 			Function: Work,
-			Schedule: &clockwork.EveryMinute,
+			Schedule: &EveryMinute,
 			Cron:     time.Now(),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "everySecond",
 			Function: Work,
-			Schedule: &clockwork.EverySecond,
+			Schedule: &EverySecond,
 			Cron:     time.Now(),
 		},
-		&clockwork.Task{
+		&Task{
 			Name:     "dayOfTheMonth",
 			Function: Work,
-			Schedule: &clockwork.DayOfMonthSchedule{
+			Schedule: &DayOfMonthSchedule{
 				DayOfMonth: 31,
 			},
 			Cron: time.Now(),
@@ -183,7 +183,7 @@ func testData() (*clockwork.Task, *clockwork.Task, *clockwork.Task, *clockwork.T
 }
 
 func testDayOfMonthScheduler() {
-	schedule := clockwork.DayOfMonthSchedule{
+	schedule := DayOfMonthSchedule{
 		DayOfMonth:        31,
 		WeekdayAdjustment: false,
 		Postpone:          false,
